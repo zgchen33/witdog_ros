e98b7489600e8c5870b7d592d12b7c9a72987815
717474 <2819591657@qq.com>
2023-03-29 10:20:14 +0800

    updata

--- servo_interface/scripts/scservo_sdk/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from .port_handler import *
-from .protocol_packet_handler import *
-from .group_sync_write import *
-from .group_sync_read import *
-from .sms_sts import *
-from .scscl import *
